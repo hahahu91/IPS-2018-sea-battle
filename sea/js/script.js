8df181a03cfbb287af526712ed5441405587c81e
Roman Morozov <hahahu91@gmail.com>
2018-11-30 14:58:36 +0300

    еще немного модулей

--- sea/js/script.js
+++ sea/js/script.js
@@ -1,10 +1,65 @@
-import {getRandomMaps} from "./inc/maps.js"; 
-import {LETTERS, NO_SHIP, NO_SHIP_INV, MISS, HAVE_SHIP, KILLED, EMPTY, END_FIELD, BEGIN_FIELD, MY_FIELD, 
+import {getRandomMap} from "./inc/maps.js"; 
+import {LETTERS, BOX_WIDTH, NO_SHIP, NO_SHIP_INV, MISS, HAVE_SHIP, KILLED, EMPTY, END_FIELD, BEGIN_FIELD, MY_FIELD, 
     ENEMY_FIELD, WIDTH_SQUARE, OFFSET_FIELD, TYPE_SHIPS, MAX_SHIPS, MY_MOVE, ENEMY_MOVE, CHECK_ALL_WAY, CHECK_ALL_DIAGANAL_SQUARE} from "./inc/consts.js";
-import {drawPlacementSquare, drawSquare} from "./inc/drawSquare.js";
+import {Game, EnemyPrevHit, startGame} from "./inc/GameController.js";
 import {markKilledShip, markWhenWounded, markDiaganalElements} from "./inc/markSquare.js";
-import  {getRandomInt, isNull, isEqualShips} from "./inc/othersFunctions.js";
-const MAP = getRandomMaps();
+import {getRandomInt, isNull, isEqualShips} from "./inc/othersFunctions.js";
+import {draw, redrawAllFields} from './inc/drawFields.js';
+function Player() {
+    this.MyMap = [
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+    ];
+    this.EnemyMap = [
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+    ];
+    this.EnemyMoves = [
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+    ];
+    this.MyShips = {
+        oneDesk: 0,
+        twoDesk: 0,
+        threeDesk: 0,
+        fourDesk: 0,
+    };
+    this.EnemyShips = {
+        oneDesk: 4,
+        twoDesk: 3,
+        threeDesk: 2,
+        fourDesk: 1,
+    };
+}
+const player1 = new Player;
+const player2 = new Player;
+player2.MyMap = getRandomMap();
+player2.MyShips = player2.EnemyShips;
+const MAP = getRandomMap();
 var MyMap = [
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
@@ -54,16 +109,7 @@ const EnemyMoves = [
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
 ]
-const Game = {
-    placement: true,
-    move: false,
-    finish: false,
-}
-const EnemyPrevHit = {
-    x: 0,
-    y: 0,
-    isHit: false,
-}
+
 
 function main () {    
     const canvas = document.getElementById('canvas');
@@ -73,8 +119,6 @@ function main () {
 	canvas.height = HEIGHT;
     const ctx = canvas.getContext('2d');
     canvas.addEventListener("mousedown", updateField, false);
-    const button = document.getElementById('begin');
-    button.addEventListener('click', checkShips, false );
     draw(ctx, WIDTH, HEIGHT);
     let lastTimestamp = Date.now();
     const animateFn = () => {
@@ -86,70 +130,7 @@ function main () {
     }
     animateFn();
 }
-function redrawAllFields(ctx, map1, map2, map3) {
-    if (Game.placement) {
-        drawMap(ctx, map1, MY_FIELD);
-        drawField2WhenPlacement(ctx);
-    } else {
-        drawMap(ctx, map2, ENEMY_FIELD);
-        drawMap(ctx, map3, MY_FIELD);
-    }
-}
-function drawField2WhenPlacement(ctx) {
-    ctx.font = "45px Mistral";    
-    ctx.fillStyle = "#4847b3";
-    ctx.textAlign = "centre";
-    ctx.textBaseline = "middle";
-    const boxWidth = 400;
-    const normalX = OFFSET_FIELD + (boxWidth + OFFSET_FIELD);
-    const normalY = OFFSET_FIELD;
 
-    outerField(ctx, normalX + 1 * WIDTH_SQUARE, normalY + 7 * WIDTH_SQUARE, 2, 4);
-    ctx.fillText("случайно", normalX + 3 * WIDTH_SQUARE, normalY + 8 * WIDTH_SQUARE - 5);
-
-    outerField(ctx, normalX + 6 * WIDTH_SQUARE, normalY + 7 * WIDTH_SQUARE, 2, 3);
-    ctx.fillText("начать", normalX + 7.5 * WIDTH_SQUARE, normalY + 8 * WIDTH_SQUARE - 5);
-
-    outerField(ctx, normalX, normalY, 10, 10);
-}
-function drawMap (ctx, map, field) {
-    ctx.font = "Bold 30pt Arial";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    const boxWidth = 400;
-    const normalX = OFFSET_FIELD + field * (boxWidth + OFFSET_FIELD);
-    const normalY = OFFSET_FIELD;
-    for(let i = 0; i < map.length; i++){
-        for (let j = 0; j < map[i].length; j++) {
-            const x = normalX + j * WIDTH_SQUARE;
-            const y = normalY + i * WIDTH_SQUARE;
-            if (Game.placement) {
-                if (field == MY_FIELD) {
-                    drawPlacementSquare(ctx, x, y, map[i][j]);
-                };
-            } else {
-                drawSquare(ctx, x, y, map[i][j]);
-            }   
-        }
-    }
-    outerField(ctx, normalX, normalY, 10, 10);
-}
-function checkShips() {
-    if (isEqualShips(MAX_SHIPS, MyShips)) {        
-        Game.placement = false;
-        let firstMove = getRandomInt(0, 2);
-        if (firstMove == 0) {
-            Game.move = MY_MOVE;
-        }  else {
-            Game.move = ENEMY_MOVE;
-            enemyMove();
-        } 
-    } else {
-        if (Game.placement == true) {
-            alert('не установленны корабли !!!');
-        }
-    }
-}
 function enemyMove() {
     let elem = AI();
     if (!Game.finish) {
@@ -286,14 +267,22 @@ function removePrevTypeShips(typeShip, lenPrevShip, ships) {
     }
 }
 function checkButtons(elem) {
-    if (elem.field == ENEMY_FIELD && elem.x >= 1 && elem.y >= 7 && elem.x <= 4 && elem.y <= 8 ) {
-        MyMap = getRandomMaps();
+    if (isClickRandomButton(elem)) {
+        MyMap = getRandomMap();
         MyShips.oneDesk = 4;
         MyShips.twoDesk = 3;
         MyShips.threeDesk = 2;
         MyShips.fourDesk = 1;
     }
-
+    if (isClickStartButton(elem)) {
+        startGame(MyShips);
+    }
+}
+function isClickRandomButton(elem) {
+    return elem.field == ENEMY_FIELD && elem.x >= 1 && elem.y >= 7 && elem.x <= 4 && elem.y <= 8;
+}
+function isClickStartButton(elem) {
+    return elem.field == ENEMY_FIELD && elem.x >= 7 && elem.y >= 7 && elem.x <= 9 && elem.y <= 8;
 }
 function updateOneField(elem) {
     if(Game.placement) {
@@ -410,76 +399,6 @@ function checkDiaganalElements(elem, map) {
     }
     return true
 }
-function createOneField(ctx, xBegin, yBegin, boxWidth) {
-    const widthSquare = WIDTH_SQUARE;
-    ctx.font = "40px Mistral";    
-    ctx.fillStyle = "#4847b3";
-    
-    for (let y = 0; y < 10; y++) {
-        for (let x = 0; x < 10; x++) {
-            ctx.fillStyle = "#4847b3";
-            if (y == BEGIN_FIELD) {
-                ctx.fillText(x + 1, xBegin + x * widthSquare + widthSquare / 2, yBegin - widthSquare + widthSquare / 2);   
-            }
-            if (x == END_FIELD) {
-                ctx.fillText(LETTERS[y], xBegin - widthSquare + widthSquare / 2, yBegin + y * widthSquare + widthSquare / 2);  
-            }
-            ctx.fillStyle = "#ffffff";
-            ctx.strokeStyle = "#b7e2e2";
-            ctx.strokeRect(xBegin + x * widthSquare, yBegin + y * widthSquare, widthSquare, widthSquare);
-            ctx.fillRect(xBegin + x * widthSquare, yBegin + y * widthSquare, widthSquare, widthSquare);
-        }
-    }
-    
-}
-function outerField(ctx, xBegin, yBegin, height, width) {
-    const widthSquare = WIDTH_SQUARE;
-    ctx.strokeStyle = "#4847b3";
-    const isVertical = true;
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-            if (y == 0) {
-                line(ctx, xBegin + x * widthSquare, yBegin);
-            }
-            if (y == height - 1) {
-                line(ctx, xBegin + x * widthSquare, yBegin + (y + 1) * widthSquare);
-            }
-            if (x == 0) {
-                line(ctx, xBegin, yBegin + y * widthSquare, isVertical);
-            }
-            if (x == width - 1) {
-                line(ctx, xBegin + (x + 1) * widthSquare, yBegin + y * widthSquare, isVertical);
-            }
-        }
-    }
-    
-}
-function line(ctx, x, y, isVertical = false) {
-    ctx.beginPath();
-    ctx.moveTo(x, y); //(controlX1, controlY1, controlX2, controlY2, endX, endY);
-    if (isVertical) {
-        ctx.bezierCurveTo(x - 2, y + 10, x + 2, y + 30, x, y + WIDTH_SQUARE + 3);
-    } else {
-        ctx.bezierCurveTo(x + 10, y - 2, x + 30, y + 2, x + WIDTH_SQUARE + 3, y);
-    }
-    ctx.lineWidth = 3;
-    ctx.stroke();
-}
-function createField(ctx, Width,  Height) {
-    ctx.fillStyle = "#ffffff";
-    ctx.strokeStyle = "#b7e2e2";
-    ctx.font = "30pt Arial";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    const x = OFFSET_FIELD;
-    const y = OFFSET_FIELD;
-    let minWidth;
-    (Width > Height) ? minWidth = Width : minWidth = Height;
-    const boxWidth = minWidth * 0.4;
-    createOneField(ctx, x, y, boxWidth);
-    createOneField(ctx, x * 2 + boxWidth, y, boxWidth);
-}
-
 function mouseCoordinates(canvas, event){
     let tempX = event.pageX - canvas.offsetLeft;
     let tempY = event.pageY - canvas.offsetTop;
@@ -487,11 +406,6 @@ function mouseCoordinates(canvas, event){
         x: tempX - OFFSET_FIELD,
         y: tempY - OFFSET_FIELD,
     }
-}
-
-
-function draw(ctx, WIDTH, HEIGHT) {
-    createField(ctx, WIDTH, HEIGHT);
 }
 
 main();
